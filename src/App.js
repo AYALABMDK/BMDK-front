03b2a53e50c5a9b37c1df56f2cc54db1d6503182
TamarAlshech1 <tamar46221@gmail.com>
2025-02-11 18:41:41 +0200

    11.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <div className="App">
       <h1>Art Talks Gallery</h1>
-      <Gallery />
     </div>
   );
 }
